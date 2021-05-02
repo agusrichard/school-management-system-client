0e17e93c6ba67a9083e3b57d39703953c7b6bc55
agusrichard <agus.richard21@gmail.com>
2021-05-02 21:44:48 +0700

    feat: register and login page

---
+++ src/components/auth-form.vue
@@ -0,0 +1,41 @@
+<template>
+  <form>
+    <h1>{{formTitle}}</h1>
+    <div class="textfield-container">
+      <TextField label='Email' placeholder='Your Email' />
+      <TextField label='Password' placeholder='Your Password' />
+    </div>
+    <Button :title='formTitle' />
+  </form>
+</template>
+
+<script>
+import Button from './button'
+import TextField from './textfield'
+
+export default {
+  components: { TextField, Button },
+  props: ['formTitle']
+}
+</script>
+
+<style scoped>
+form {
+  width: 50%;
+  display: flex;
+  padding-top: 50px;
+  align-items: center;
+  border-radius: 10px;
+  padding-bottom: 75px;
+  background: #efefef;
+  flex-direction: column;
+}
+
+h1 {
+  font-size: 28px;
+}
+
+.textfield-container {
+  padding: 30px 0;
+}
+</style>
---
+++ src/components/textfield.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-text-field
+    outlined
+    :label="label"
+    class="textfield"
+    :placeholder="placeholder"
+  ></v-text-field>
+</template>
+
+<script>
+export default {
+  props: ['label', 'placeholder']
+}
+</script>
+
+<style>
+.textfield {
+  width: 350px;
+}
+</style>
